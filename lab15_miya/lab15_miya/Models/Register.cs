2071807f26541afe6c8129f2141fda73cff27d9e
miyaluc <srlucas3@gmail.com>
2017-10-02 14:06:00 -0700

    lab deployed to Azure

---
+++ lab15_miya/lab15_miya/Models/Register.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab15_miya.Models
+{
+    public class Register
+    {
+        public int ID
+        {
+            get; set;
+        }
+        [StringLength(30, MinimumLength = 1)]
+        [Required]
+        public string Name
+        {
+            get; set;
+        }
+        [Range(1, 130)]
+        [Required]
+        public int Age
+        {
+            get; set;
+        }
+        [Range(1, 12)]
+        public int Grade
+        {
+            get; set;
+        }
+        [StringLength(30)]
+        public string Talent
+        {
+            get; set;
+        }
+        [Display(Name = "Favorite Subject")]
+        public string FavoriteClass
+        {
+            get; set;
+        }
+    }
+}
